54985af6dc254842a87e545457347b3e4de74043
Jordan <jordanskesner@gmail.com>
2025-06-26 20:50:10 -0400

    some code refactoring and updated detect converging genes function to allow for overlapping or non-overlapping pairs

--- pipeline_utils.py
+++ pipeline_utils.py
@@ -6,6 +6,8 @@ import requests
 from datetime import datetime
 import sys
 from contextlib import redirect_stdout
+import pandas as pd
+import gzip
 
 #=== Function to download genome sequences and annotations ===#
 def download_genome(genome_info, output_dir):
@@ -178,3 +180,111 @@ def download_file(url, dest_path, verbose=True):
         else:
             print(f"Failed to download {url} - status code {response.status_code}")
 
+
+#=== PARSE GFF3 File ===#
+def parse_gff3_genes(gff3_path, verbose = False):
+
+    ##
+    temp_data = []
+
+    ##
+    with gzip.open(gff3_path, 'rt') as f:
+        ##
+        for line in f:
+            ##
+            if line.startswith("#"):
+                if verbose:
+                    print("Skipping line starting with ##")
+                continue
+    
+            ##
+            fields = line.strip().split("\t")
+            #print(f"Current fields: {fields}")
+    
+            ##
+            if len(fields) < 9:
+                if verbose:
+                    print("Incomplete record, skipping")
+                continue
+    
+            ##
+            chrom, source, feature, start, end, score, strand, phase, attributes = fields
+            
+            if feature != "gene":
+                if verbose:
+                    print("Feature is not gene, skipping")
+                continue
+    
+            ##
+            attr_dict = {kv.split("=")[0]: kv.split("=")[1] for kv in attributes.split(";") if "=" in kv}
+            gene_ID = attr_dict.get("ID", "unknown")
+            gene_name = attr_dict.get("Name", "unknown")
+
+            ##
+            temp = {
+                "id":gene_ID,
+                "gene":gene_name,
+                "chrom":chrom,
+                "start":start,
+                "end":end,
+                "strand":strand
+            }
+                
+            temp_data.append(temp)
+            
+    ## Convert to dataframe
+    genes = pd.DataFrame(temp_data)
+
+    ## Return
+    return(genes)
+
+
+#=== Find converging gene pairs ===#
+def find_facing_gene_pairs(genes, max_distance, exclude_overlap=False, verbose=False):
+
+    if verbose:
+        print(f"Called find_facing_gene_pairs function with the following parameters:")
+        print(f"Max distance between genes ends to be considered converging: {max_distance}")
+        print(f"Excluding overlapping genes: {exclude_overlap}")
+
+    ## instantiate 
+    genes_by_chrom = {}
+    close_pairs = []
+
+    ## sort genes into dictionary by chromosome
+    for _, row in genes.iterrows():
+        genes_by_chrom.setdefault(row['chrom'], []).append(row.to_dict())
+
+    ## loop over all chromosome:gene list pairs
+    for chrom, chrom_genes in genes_by_chrom.items():
+        ## sort genes on each chromosome by starting position
+        sorted_genes = sorted(chrom_genes, key=lambda g: g['start'])
+
+    ## iterate through each pair of adjacent genes
+        for i in range(len(sorted_genes) - 1):
+            g1 = sorted_genes[i]
+            g2 = sorted_genes[i + 1]
+            distance = int(g2['start']) - int(g1['end'])
+            ## 
+            if exclude_overlap:
+                if 0 <= distance <= max_distance:
+                    # Check if they face each other
+                    if g1['strand'] == '+' and g2['strand'] == '-':
+                        close_pairs.append((g1, g2))
+                    elif g1['strand'] == '-' and g2['strand'] == '+':
+                        close_pairs.append((g2, g1))
+            ##
+            else:
+                if distance <= max_distance:
+                    # Check if they face each other
+                    if g1['strand'] == '+' and g2['strand'] == '-':
+                        close_pairs.append((g1, g2))
+                    elif g1['strand'] == '-' and g2['strand'] == '+':
+                        close_pairs.append((g2, g1))
+
+    ## log output
+    if verbose:
+        print(f"Found {len(close_pairs)} converging gene pairs")
+    
+    ## return
+    return(close_pairs)
